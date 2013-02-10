2dcefcb50e69008e7994fb2559a40a9ca87b5bd6
Tudor <vulcainus@gmail.com>
2013-02-10 11:37:19 -0500

    Removed incremental loading and updated Metadatable implementation

--- src/main/java/tk/jblib/bytecode/introspection/ClassFile.java
+++ src/main/java/tk/jblib/bytecode/introspection/ClassFile.java
@@ -4,22 +4,20 @@ import tk.jblib.bytecode.introspection.members.Attribute;
 import tk.jblib.bytecode.introspection.members.Constant;
 import tk.jblib.bytecode.introspection.members.Interface;
 import tk.jblib.bytecode.introspection.metadata.Metadatable;
-import tk.jblib.bytecode.introspection.metadata.XMLAttribute;
 import tk.jblib.bytecode.util.ByteStream;
 import tk.jblib.bytecode.util.Bytes;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Collection;
 
 import static tk.jblib.bytecode.introspection.Opcode.TAG_UTF_STRING;
 
 /**
  * A class file structure, used for introspection and modification of classes.
  */
-public class ClassFile extends AccessibleMember implements Metadatable<Attribute> {
+public class ClassFile extends AccessibleMember implements Metadatable<Attribute>{
 
     protected int majorVersion;
     protected int minorVersion;
@@ -29,9 +27,7 @@ public class ClassFile extends AccessibleMember implements Metadatable<Attribute
     protected Pool<Interface> interfacePool;
     protected Pool<Member> fieldPool;
     protected Pool<Member> methodPool;
-    protected Pool<Attribute> attributePool;
-    protected ByteStream classStream;
-    private int loaded;
+    protected Metadatable.Container metadata;
 
     /**
      * Constructs a class file object.
@@ -39,9 +35,23 @@ public class ClassFile extends AccessibleMember implements Metadatable<Attribute
      * @param bytes The bytes to construct this object from.
      */
     public ClassFile(byte[] bytes) {
-        classStream = new ByteStream(bytes);
-        if (classStream.readInt() != 0xCAFEBABE)
-            throw new IllegalStateException("File does not contain magic number 0xCAFEBABE");
+        ByteStream in = new ByteStream(bytes);
+        if (in.readInt() != 0xCAFEBABE)
+            throw new ClassFormatError("File does not contain magic number 0xCAFEBABE");
+
+        minorVersion = in.readShort();
+        majorVersion = in.readShort();
+
+        constantPool = new Pool<Constant>(in, Pool.CONSTANT_PARSER);
+
+        flag = in.readShort();
+        thisClass = constantPool.get(in.readShort());
+        superClass = constantPool.get(in.readShort());
+
+        interfacePool = new Pool<Interface>(in, Pool.INTERFACE_PARSER, constantPool);
+        fieldPool = new Pool<Member>(in, Pool.MEMBER_PARSER, constantPool);
+        methodPool = new Pool<Member>(in, Pool.MEMBER_PARSER, constantPool);
+        metadata = new Metadatable.Container(new Pool<Attribute>(in, Pool.ATTRIBUTE_PARSER, constantPool), constantPool);
     }
 
     /**
@@ -68,27 +78,16 @@ public class ClassFile extends AccessibleMember implements Metadatable<Attribute
     public byte[] getBytes() {
         ByteStream out = new ByteStream();
         out.write(Bytes.toByteArray(0xCAFEBABE));
-        if (loaded >= 1) {
-            out.write(Bytes.toByteArray((short)minorVersion));
-            out.write(Bytes.toByteArray((short)majorVersion));
-        }
-        if (loaded >= 2)
-            out.write(constantPool.getBytes());
-        if (loaded >= 3) {
-            out.write(Bytes.toByteArray((short) flag));
-            out.write(Bytes.toByteArray((short) thisClass.getIndex()));
-            out.write(Bytes.toByteArray((short) superClass.getIndex()));
-        }
-        if (loaded >= 4)
-            out.write(interfacePool.getBytes());
-        if (loaded >= 5)
-            out.write(fieldPool.getBytes());
-        if (loaded >= 6)
-            out.write(methodPool.getBytes());
-        if (loaded >= 7)
-            out.write(attributePool.getBytes());
-        if (loaded < 7)
-            out.write(classStream.readFully()); //Read the rest of the class
+        out.write(Bytes.toByteArray((short) minorVersion));
+        out.write(Bytes.toByteArray((short) majorVersion));
+        out.write(constantPool.getBytes());
+        out.write(Bytes.toByteArray((short) flag));
+        out.write(Bytes.toByteArray((short) thisClass.getIndex()));
+        out.write(Bytes.toByteArray((short) superClass.getIndex()));
+        out.write(interfacePool.getBytes());
+        out.write(fieldPool.getBytes());
+        out.write(methodPool.getBytes());
+        out.write(metadata.getAttributes().getBytes());
         return out.toByteArray();
     }
 
@@ -111,7 +110,6 @@ public class ClassFile extends AccessibleMember implements Metadatable<Attribute
      * @return the major version of this class.
      */
     public int getMajorVersion() {
-        ensureLoaded(1);
         return majorVersion;
     }
 
@@ -130,7 +128,6 @@ public class ClassFile extends AccessibleMember implements Metadatable<Attribute
      * @return the minor version of this class.
      */
     public int getMinorVersion() {
-        ensureLoaded(1);
         return minorVersion;
     }
 
@@ -149,7 +146,6 @@ public class ClassFile extends AccessibleMember implements Metadatable<Attribute
      * @return a constant pool.
      */
     public Pool<Constant> getConstantPool() {
-        ensureLoaded(2);
         return constantPool;
     }
 
@@ -168,7 +164,6 @@ public class ClassFile extends AccessibleMember implements Metadatable<Attribute
      * @return the fully qualified name of this class.
      */
     public String getName() {
-        ensureLoaded(3);
         return thisClass.stringValue();
     }
 
@@ -178,7 +173,6 @@ public class ClassFile extends AccessibleMember implements Metadatable<Attribute
      * @param clazz The new name for this class.
      */
     public void setName(String clazz) {
-        ensureLoaded(3);
         thisClass.getOwner().set(thisClass.getIndex(), (thisClass = new Constant(thisClass.getIndex(), TAG_UTF_STRING, clazz.getBytes())));
     }
 
@@ -188,7 +182,6 @@ public class ClassFile extends AccessibleMember implements Metadatable<Attribute
      * @return the fully qualified name of this class' superclass.
      */
     public String getSuperClass() {
-        ensureLoaded(3);
         return superClass.stringValue();
     }
 
@@ -198,7 +191,6 @@ public class ClassFile extends AccessibleMember implements Metadatable<Attribute
      * @param superclass The new name for this class' superclass.
      */
     public void setSuperClass(String superclass) {
-        ensureLoaded(3);
         superClass.getOwner().set(superClass.getIndex(), (superClass = new Constant(superClass.getIndex(), TAG_UTF_STRING, superclass.getBytes())));
     }
 
@@ -208,7 +200,6 @@ public class ClassFile extends AccessibleMember implements Metadatable<Attribute
      * @return an interface pool.
      */
     public Pool<Interface> getInterfacePool() {
-        ensureLoaded(4);
         return interfacePool;
     }
 
@@ -227,7 +218,6 @@ public class ClassFile extends AccessibleMember implements Metadatable<Attribute
      * @return a field pool.
      */
     public Pool<Member> getFieldPool() {
-        ensureLoaded(5);
         return fieldPool;
     }
 
@@ -246,7 +236,6 @@ public class ClassFile extends AccessibleMember implements Metadatable<Attribute
      * @return a method pool.
      */
     public Pool<Member> getMethodPool() {
-        ensureLoaded(6);
         return methodPool;
     }
 
@@ -265,8 +254,7 @@ public class ClassFile extends AccessibleMember implements Metadatable<Attribute
      * @return a method pool.
      */
     public Pool<Attribute> getAttributePool() {
-        ensureLoaded(7);
-        return attributePool;
+        return metadata.getAttributes();
     }
 
     /**
@@ -275,82 +263,31 @@ public class ClassFile extends AccessibleMember implements Metadatable<Attribute
      * @param attributePool the pool to set it to.
      */
     public void setAttributePool(Pool<Attribute> attributePool) {
-        this.attributePool = attributePool;
+        metadata.setAttributes(attributePool);
     }
 
-    public void ensureLoaded(int pos) {
-        if (pos > loaded) {
-            switch (pos) {
-                case 1:
-                    minorVersion = classStream.readShort();
-                    majorVersion = classStream.readShort();
-                    break;
-                case 2:
-                    ensureLoaded(1);
-                    constantPool = new Pool<Constant>(classStream, Pool.CONSTANT_PARSER);
-                    break;
-                case 3:
-                    ensureLoaded(2);
-                    flag = classStream.readShort();
-                    thisClass = constantPool.get(classStream.readShort());
-                    superClass = constantPool.get(classStream.readShort());
-                    break;
-                case 4:
-                    ensureLoaded(3);
-                    interfacePool = new Pool<Interface>(classStream, Pool.INTERFACE_PARSER, constantPool);
-                    break;
-                case 5:
-                    ensureLoaded(4);
-                    fieldPool = new Pool<Member>(classStream, Pool.MEMBER_PARSER, constantPool);
-                    break;
-                case 6:
-                    ensureLoaded(5);
-                    methodPool = new Pool<Member>(classStream, Pool.MEMBER_PARSER, constantPool);
-                    break;
-                case 7:
-                    ensureLoaded(6);
-                    attributePool = new Pool<Attribute>(classStream, Pool.ATTRIBUTE_PARSER, constantPool);
-                    break;
-                default:
-                    classStream = null; //Clean up, at least partially
-                    return;
-            }
-            loaded++;
-        }
+    @Override
+    public Collection<Attribute> getMetadataInstances(String meta) {
+        return metadata.getAttributes();
     }
 
-    public Set<Attribute> getMetadataInstances(String meta) {
-        Set<Attribute> out = new HashSet<Attribute>();
-        for (Attribute a : attributePool)
-            if (a.getName().equals(meta))
-                out.add(a);
-        return out;
-    }
-
+    @Override
     public void removeMetadata(String meta) {
-        attributePool.removeAll(getMetadataInstances(meta));
+        metadata.removeMetadata(meta);
+    }
+
+    @Override
+    public Object getMetadata(String meta) {
+        return metadata.getMetadata(meta);
     }
 
     @Override
     public boolean hasMetadata(String meta) {
-        return getMetadataInstances(meta).size() > 0;
-    }
-
-    public Object getMetadata(String meta) {
-        Attribute ab = getMetadataInstances(meta).iterator().next();
-        return ab instanceof XMLAttribute ? ((XMLAttribute) ab).readObject() : ab;
+        return metadata.hasMetadata(meta);
     }
 
     @Override
     public <V> void addMetadata(String meta, V value) {
-        if (value instanceof Attribute)
-            attributePool.add((Attribute) value);
-        else {
-            try {
-                attributePool.add(new XMLAttribute<V>(meta, value));
-            } catch (IOException e) {
-                throw new RuntimeException("could not encode attribute '" + meta + "' with value '" + value + "'", e);
-            }
-        }
+        metadata.addMetadata(meta, value);
     }
 }
